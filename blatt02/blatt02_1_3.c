5cd09a505c8244063fb8b1b8424b1e9ab7c1f8bb
Leonard Kugis <leonardkugis@gmail.com>
2017-11-01 17:32:34 +0100

    blatt02_1_3.c fertiggestellt

--- blatt02/blatt02_1_3.c
+++ blatt02/blatt02_1_3.c
@@ -2,13 +2,13 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define PRINT_ROW(y) \
-	for (u8 i = 0; i < n-y; i++) \
-		printf(" "); \
-	for (u8 x = 1; x <= y; x++) \
-		printf("%d", x); \
-	for (u8 x = y-1; x > 0; x--) \
-		printf("%d", x); \
+#define PRINT_ROW(y)				\
+	for (u8 i = 0; i < n-y; i++)	\
+		printf(" ");				\
+	for (u8 x = 1; x <= y; x++)		\
+		printf("%d", x);			\
+	for (u8 x = y-1; x > 0; x--)	\
+		printf("%d", x);			\
 	printf("\n");
 
 typedef uint16_t u16;
@@ -24,18 +24,10 @@ int main(int argc, char **argv) {
 		{
 			PRINT_ROW(y);
 		}
-		for (u8 y = 1; y <= n; y++)
+		for (u8 y = n-1; y >= 1; y--)
 		{
-			PRINT_ROW(n-y);
+			PRINT_ROW(y);
 		}
 	}
 	return 0;
 }
-
-/*
-  1
- 121
-12321
- 121
-  1
-*/
