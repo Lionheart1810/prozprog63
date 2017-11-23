028cd3ea1a8eb2e68fa6aa653b50309cc08e8cc8
Leonard Kugis <leonardkugis@gmail.com>
2017-11-23 15:34:33 +0100

    Improved generation, console clearance, OS wrapper, bugfixes

--- blatt05/blatt05_1.c
+++ blatt05/blatt05_1.c
@@ -2,10 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define DEBUG 0
+#define _FOR_WINDOWS
+
 #define TRUE 1
 #define FALSE 0
 
 #define CHECK_ELEMENT(x, y) if (sudoku[y][x] != 0) { if ((checker >> (sudoku[y][x]-1)) & 1) return FALSE; else checker |= (1 << (sudoku[y][x]-1)); }
+#define SUDOKU_READ(x, y) (sudoku[y][x] & 0xF)
+
+#if DEBUG == 1
+#define DLOG(x) printf(x)
+#else
+#define DLOG(x) 
+#endif
+
+#ifdef _FOR_LINUX
+#define CONSOLE_CLEAR system("clear")
+#endif
+#ifdef _FOR_WINDOWS
+#define CONSOLE_CLEAR system("cls")
+#endif
 
 typedef unsigned char u8;
 typedef unsigned short u16;
@@ -35,70 +52,71 @@ u8 sudoku[9][9] = { { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 
 u8 check(u8 x, u8 y, u8 n)
 {
-	u8 n_old = sudoku[y][x];
-	sudoku[y][x] = n;
-	u16 checker = 0;
-	// check row
-	for (u8 i = 0; i < 9; i++)
-	{
-		if (sudoku[y][i] != 0)
-		{
-			if ((checker >> (sudoku[y][i]-1)) & 1)
-			{
-				goto failed;
-			}
-			else
-				checker |= (1 << (sudoku[y][i]-1));
-		}
-	}
-	// check column
-	checker = 0;
-	for (u8 i = 0; i < 9; i++)
-	{
-		if (sudoku[i][x] != 0)
-		{
-			if ((checker >> (sudoku[i][x]-1)) & 1)
-			{
-				goto failed;
-			}
-			else
-				checker |= (1 << (sudoku[i][x]-1));
-		}
-	}
-	// check block
-	checker = 0;
-	for (u8 by = y-(y%3), zy = y-(y%3)+3; by < zy; by++)
-	{
-		for (u8 bx = x-(x%3), zx = x-(x%3)+3; bx < zx; bx++)
-		{
-			if (sudoku[by][bx] != 0)
-			{
-				if ((checker >> (sudoku[by][bx]-1)) & 1)
-				{
-					goto failed;
-				}
-				else
-					checker |= (1 << (sudoku[by][bx]-1));
-			}
-		}
-	}
-	sudoku[y][x] = n_old;
-	return TRUE;
-	failed: sudoku[y][x] = n_old;
-	return FALSE;
+        u8 n_old = sudoku[y][x];
+        sudoku[y][x] = n;
+        u16 checker = 0;
+        // check row
+        for (u8 i = 0; i < 9; i++)
+        {
+            if (SUDOKU_READ(i, y) != 0)
+            {
+                if ((checker >> (SUDOKU_READ(i, y)-1)) & 1)
+                {
+                    goto failed;
+                }
+                else
+                    checker |= (1 << (SUDOKU_READ(i, y)-1));
+            }
+        }
+        // check column
+        checker = 0;
+        for (u8 i = 0; i < 9; i++)
+        {
+            if (SUDOKU_READ(x, i) != 0)
+            {
+                if ((checker >> (SUDOKU_READ(x, i)-1)) & 1)
+                {
+                    goto failed;
+                }
+                else
+                    checker |= (1 << (SUDOKU_READ(x, i)-1));
+            }
+        }
+        // check block
+        checker = 0;
+        for (u8 by = y-(y%3), zy = y-(y%3)+3; by < zy; by++)
+        {
+            for (u8 bx = x-(x%3), zx = x-(x%3)+3; bx < zx; bx++)
+            {
+                if (SUDOKU_READ(bx, by) != 0)
+                {
+                    if ((checker >> (SUDOKU_READ(bx, by)-1)) & 1)
+                    {
+                        goto failed;
+                    }
+                    else
+                        checker |= (1 << (SUDOKU_READ(bx, by)-1));
+                }
+            }
+        }
+        sudoku[y][x] = n_old;
+        return TRUE;
+        failed: sudoku[y][x] = n_old;
+        DLOG("durchgefallen\n");
+        return FALSE;
 }
 
 void set()
 {
 	u32 x, y, n;
 	printf("X: ");
-	scanf("%hhu", &x);
+	scanf("%lu", &x);
 	printf("Y: ");
-	scanf("%hhu", &y);
+	scanf("%lu", &y);
 	printf("Number: ");
-	scanf("%hhu", &n);
+	scanf("%lu", &n);
 	x--; y--;
-	if (check(x, y, n))
+	if ((!(sudoku[y][x] >> 7)) && check(x, y, n))
 	{
 		struct placement *e = malloc(sizeof(struct placement));
 		e->x = x;
@@ -122,9 +140,9 @@ void set()
 
 void undo()
 {
-	u8 t;
+	u32 t;
 	printf("Undo how many steps: ");
-	scanf("%d", &t);
+	scanf("%lu", &t);
 	for (u32 i = 0; i < t; i++)
 	{
 		if (undo_ptr)
@@ -150,7 +168,7 @@ void undo()
 
 void redo()
 {
-	u8 t;
+	u32 t;
 	printf("Redo how many steps: ");
 	scanf("%lu", &t);
 	for (u32 i = 0; i < t; i++)
@@ -198,21 +216,24 @@ u8 interpret(char *seq)
 		printf("Couldnt interpret sequence, type h[e[l[p]]] for more info.\n");
 		break;
 	}
+    getchar();
+    getchar();
 	return TRUE;
 }
 
 void init(u32 filled)
 {
+    DLOG("init\n");
 	u8 r = 0;
 	srand(time(NULL));
 	for (u8 y = 0; y < 9; y++)
 	{
 		for (u8 x = 0; x < 9; x++)
 		{
-			if (((u32)rand() % 100) <= filled)
+			if (((u32)rand() % 100) < filled)
 			{
-				while (!check(x, y, r = (u8)rand() % 9));
-				sudoku[y][x] = r;
+				while (!check(x, y, r = ((u8)rand() % 9) + 1));
+				sudoku[y][x] = r | (1 << 7);
 			}
 		}
 	}
@@ -237,6 +258,7 @@ int main(int argc, char **argv)
 	init((argc == 2) ? atoi(argv[1]) : 50);
 	while (TRUE)
 	{
+        CONSOLE_CLEAR;
 		// Ausgabe des Spielfeldes
 		printf("  | 1 2 3 | 4 5 6 | 7 8 9 |\n--+-------+-------+-------+\n");
 		for (u8 y = 0; y < 9; y++)
@@ -244,7 +266,7 @@ int main(int argc, char **argv)
 			printf("%d ", y+1);
 			for (u8 x = 0; x < 9; x++)
 			{
-				printf((x % 3 == 0) ? "| %d " : "%d ", sudoku[y][x]);
+				printf((x % 3 == 0) ? "| %d " : "%d ", SUDOKU_READ(x, y));
 			}
 			printf(((y+1) % 3 == 0) ? "|\n--+-------+-------+-------+\n" : "|\n");
 		}
